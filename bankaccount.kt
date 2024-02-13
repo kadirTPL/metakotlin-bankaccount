3cc8eafd343f2c6ce2c6134b5dcda25f2c45027a
kadirTPL <omkadenec_topal@windowslive.com>
2024-02-13 15:36:21 +0300

    part3

--- bankaccount.kt
+++ bankaccount.kt
@@ -1,4 +1,6 @@
 fun main(){
+
+
     var accountType=""
     var userChoice=0
     println("Welcome to your banking system.")
@@ -26,20 +28,20 @@ fun main(){
     println("Enter your balance:")
 var accountBalance= readln().toInt()
 println("The current balance is $accountBalance dollars.")
-val money= readln().toInt()
+
 fun withdraw(amount:Int):Int {
-        println("You succesfully withdrew $amount dollars.")
+    accountBalance =accountBalance-amount
+    println("You successfully withdrew $amount dollars.")
     println("Your current balance is:$accountBalance")
-        accountBalance -= amount
-    return accountBalance
+    return amount
 }
 fun deposit(amount:Int):Int{
-    accountBalance+=amount
-    println("You succesfully deposited $amount dollars.")
+    accountBalance= accountBalance+amount
+    println("You successfully deposited $amount dollars.")
     println("Your current balance is:$accountBalance")
-    return accountBalance
+    return amount
 }
-fun debitWithdraw(amount:Int):Any{
+fun debitWithdraw(amount:Int):Int{
     if (amount > accountBalance) {
         println("Not enough money on this account! The current balance is ${accountBalance} dollars.")
         return 0
@@ -48,7 +50,7 @@ fun debitWithdraw(amount:Int):Any{
     }
 }
 fun creditDeposit(amount:Int):Int{
-    if(accountBalance + amount > 0){
+    if(-accountBalance + amount > 0){
         println("Can not deposit. You are trying to deposit more money than your credit balance.")
         return 0
     }
@@ -56,6 +58,57 @@ fun creditDeposit(amount:Int):Int{
         return deposit(amount)
     }
 }
+fun transfer (mode:String){
+    println("Please enter the transfer amount:")
+    val money= readln().toInt()
+    val transferAmount:Int
+    when(mode){
+    "withdraw"->{
+        if(accountType.equals("debit"))
+            transferAmount=debitWithdraw(money)
+        else
+            transferAmount=withdraw(money)
+    }
+    "deposit"->{
+        if(accountType.equals("credit"))
+            transferAmount=creditDeposit(money)
+        else
+            transferAmount=deposit(money)
+    }
+    else->{
+        return
+    }
+        }
+}
+    var isSystemOpen=true
+    var option=0
+    while(isSystemOpen){
+        println("What would you like to do?")
+        println("1. Check bank account balance")
+        println("2. Withdraw money")
+        println("3. Deposit money")
+        println("4. Close the app")
+        println("Which option do you choose? (1, 2, 3 or 4)")
+        var option=readln().toInt()
+        when(option){
+            1->println("Your balance is:$accountBalance")
+            2->transfer("withdraw")
+            3->transfer("deposit")
+            4->isSystemOpen=false
+            else->continue
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
 
 
 }
