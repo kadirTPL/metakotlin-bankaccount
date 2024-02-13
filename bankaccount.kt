f35ef65942cb9e5bec905d09f660dca5b570169a
kadirTPL <omkadenec_topal@windowslive.com>
2024-02-13 15:01:48 +0300

    part2

--- bankaccount.kt
+++ bankaccount.kt
@@ -23,7 +23,39 @@ fun main(){
 
         }}
     println("You have created a $accountType account.")
-
+    println("Enter your balance:")
+var accountBalance= readln().toInt()
+println("The current balance is $accountBalance dollars.")
+val money= readln().toInt()
+fun withdraw(amount:Int):Int {
+        println("You succesfully withdrew $amount dollars.")
+    println("Your current balance is:$accountBalance")
+        accountBalance -= amount
+    return accountBalance
+}
+fun deposit(amount:Int):Int{
+    accountBalance+=amount
+    println("You succesfully deposited $amount dollars.")
+    println("Your current balance is:$accountBalance")
+    return accountBalance
+}
+fun debitWithdraw(amount:Int):Any{
+    if (amount > accountBalance) {
+        println("Not enough money on this account! The current balance is ${accountBalance} dollars.")
+        return 0
+    } else {
+        return withdraw(amount)
+    }
+}
+fun creditDeposit(amount:Int):Int{
+    if(accountBalance + amount > 0){
+        println("Can not deposit. You are trying to deposit more money than your credit balance.")
+        return 0
+    }
+    else {
+        return deposit(amount)
+    }
+}
 
 
 }
